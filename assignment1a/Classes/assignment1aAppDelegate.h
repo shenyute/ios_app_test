8499a6b84acc80c2b4ef2621723af6671f699f0d
jeremych <shenyute@gmail.com>
2012-02-28 17:18:27 +0800

    assignment1a for hello world

---
+++ assignment1a/Classes/assignment1aAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  assignment1aAppDelegate.h
+//  assignment1a
+//
+//  Created by 張翊新 on 2012/2/28.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface assignment1aAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
