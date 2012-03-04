2e3162016fcee39c8420dfaf216ace41d9d823bf
jeremych <shenyute@gmail.com>
2012-03-04 10:10:33 +0800

    Initial project with window-base application

---
+++ HelloPoly/Classes/HelloPolyAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  HelloPolyAppDelegate.h
+//  HelloPoly
+//
+//  Created by 張翊新 on 2012/3/4.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloPolyAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
