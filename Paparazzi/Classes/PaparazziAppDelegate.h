4ce3ccc2ebed44b7d5f701d39d505a99b3f7d419
jeremych <shenyute@gmail.com>
2012-03-28 07:52:03 +0800

    Paparizzi init

---
+++ Paparazzi/Classes/PaparazziAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  PaparazziAppDelegate.h
+//  Paparazzi
+//
+//  Created by 張翊新 on 2012/3/28.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaparazziAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
